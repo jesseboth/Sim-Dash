6a74f60cf4b124808334791a1fe3afd1a0440128
Jesse Both <jesse.both@biamp.com>
2026-02-18 22:00:57 -0500

    -

--- telemetry/src/util/json-server.go
+++ telemetry/src/util/json-server.go
@@ -68,5 +68,7 @@ func SetJson(str string) {
 
 func enableCors(w *http.ResponseWriter) {
     (*w).Header().Set("Access-Control-Allow-Origin", "*")
+    (*w).Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+    (*w).Header().Set("Access-Control-Allow-Headers", "Content-Type")
 }
 
